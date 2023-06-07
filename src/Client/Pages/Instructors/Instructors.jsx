f0b889db1ee92566132d09f7907ceb7e4b6b9f2d
joySutradhor <joysutradhorcmt@gmail.com>
2023-06-07 22:33:24 +0600

    react hook form install for register and login page

---
+++ src/Client/Pages/Instructors/Instructors.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Instructors = () => {
+    return (
+        <div>
+            <p>instructors page is  here</p>
+        </div>
+    );
+};
+
+export default Instructors;
--- src/Client/Shared/Navbar.jsx
+++ src/Client/Shared/Navbar.jsx
@@ -1,35 +1,44 @@
 import { Link } from "react-router-dom";
+import userPhoto from "../../assets/user.png"
 
 
 const Navbar = () => {
-    const options = <>
-     <li> <Link to="/">Home</Link> </li>
-     <li> <Link to="/menu">Menu</Link> </li>
-     <li> <Link to="/order">Order</Link> </li>
-              </>
-    return (
-        <div className="navbar fixed z-10  bg-base-100 ">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              {options}
-            </ul>
-          </div>
-          <a >My Kitchen</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+  const options = <>
+    <li> <Link to="/">Home</Link> </li>
+    <li> <Link to="/instructors">Instructors</Link> </li>
+    <li> <Link to="/classes">Classes</Link> </li>
+    <li> <Link to="/dashboard">Dashboard</Link> </li>
+  </>
+  return (
+    <div className="navbar fixed z-10  bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
             {options}
           </ul>
         </div>
-        <div className="navbar-end mr-[180px]">
-          <a className="btn">Get started</a>
+        <a >Cricket World</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {options}
+        </ul>
+      </div>
+      <div className="navbar-end md:mr-[145px] mr-10">
+        <div className="flex gap-5">
+          <div>
+            <button>Log Out </button>
+          </div>
+          <div>
+            <img className="w-[30px] h-[30px] rounded-md" src={userPhoto} alt="" />
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
